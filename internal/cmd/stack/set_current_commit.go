4df338dbddd54f9d17944ee0aaf14d555cf14097
Marcin Wyszynski <marcinw@spacelift.io>
2021-05-10 22:36:15 +0200

    Command to set current stack commit (#13)

---
+++ internal/cmd/stack/set_current_commit.go
@@ -0,0 +1,44 @@
+package stack
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/shurcooL/graphql"
+	"github.com/urfave/cli/v2"
+
+	"github.com/spacelift-io/spacectl/internal/cmd/authenticated"
+)
+
+func setCurrentCommit(cliCtx *cli.Context) error {
+	var mutation struct {
+		SetCurrentCommmit struct {
+			TrackedCommit *struct {
+				Hash    string `graphql:"hash"`
+				Message string `graphql:"message"`
+			} `grapqhl:"trackedCommit"`
+		} `graphql:"stackSetCurrentCommit(id: $stack, sha: $sha)"`
+	}
+
+	variables := map[string]interface{}{
+		"sha":   graphql.String(cliCtx.String(flagRequiredCommitSHA.Name)),
+		"stack": graphql.ID(stackID),
+	}
+
+	ctx := context.Background()
+
+	if err := authenticated.Client.Mutate(ctx, &mutation, variables); err != nil {
+		return err
+	}
+
+	commit := mutation.SetCurrentCommmit.TrackedCommit
+	if commit == nil {
+		return errors.New("no tracked commit set on the Stack")
+	}
+
+	_, err := fmt.Fprintf(os.Stdout, "Current commit set to %q: (SHA %s)", commit.Message, commit.Hash)
+
+	return err
+}
