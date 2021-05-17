910c2f58bf82c3b39d47165638af3bcc8dcbf43a
Jakub Martin <jakub.wit.martin@gmail.com>
2021-05-17 14:11:44 +0200

    Fix lint.

--- internal/cmd/stack/constants.go
+++ internal/cmd/stack/constants.go
@@ -1,3 +1,5 @@
 package stack
 
+// UserProvidedRunMetadataHeader is the HTTP header used to pass arbitrary metadata
+// to runs when creating or confirming them.
 const UserProvidedRunMetadataHeader = "Spacelift-User-Provided-Run-Metadata"
