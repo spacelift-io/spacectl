6711766c3d6ecd46ec004baa0af33270c2b1dfca
Chris Spitzenberger <chriss@spacelift.io>
2022-05-15 11:31:29 -0500

    Change version command into a flag

--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/urfave/cli/v2"
 
 	"github.com/spacelift-io/spacectl/internal/cmd/profile"
 	"github.com/spacelift-io/spacectl/internal/cmd/stack"
-	versioncmd "github.com/spacelift-io/spacectl/internal/cmd/version"
 )
 
 var version = "dev"
 
 func main() {
 	app := &cli.App{
-		Name:  "spacectl",
-		Usage: "Programmatic access to Spacelift GraphQL API",
+		Name:     "spacectl",
+		Version:  version,
+		Compiled: time.Now(),
+		Usage:    "Programmatic access to Spacelift GraphQL API.",
 		Commands: []*cli.Command{
 			profile.Command(),
 			stack.Command(),
-			versioncmd.Command(version),
 		},
 	}
 
