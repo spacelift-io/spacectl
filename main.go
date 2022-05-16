d55ef3644d319e1f61d270cc6f55a4bc928cc60b
Chris Spitzenberger <43446469+spitzzz@users.noreply.github.com>
2022-05-16 11:25:07 -0500

    Add version and compile time information (now support the --version flag)(#61)
    * Added version and compiled attributes to the app (now support using the --version flag)

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"time"
+	"fmt"
 
 	"github.com/urfave/cli/v2"
 
@@ -12,11 +14,18 @@ import (
 )
 
 var version = "dev"
+var date = "2006-01-02T15:04:05Z"
 
 func main() {
+	compileTime, err := time.Parse(time.RFC3339, date)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not parse compilation date: %v", err))
+	}
 	app := &cli.App{
-		Name:  "spacectl",
-		Usage: "Programmatic access to Spacelift GraphQL API",
+		Name:     "spacectl",
+		Version:  version,
+		Compiled: compileTime,
+		Usage:    "Programmatic access to Spacelift GraphQL API.",
 		Commands: []*cli.Command{
 			profile.Command(),
 			stack.Command(),
