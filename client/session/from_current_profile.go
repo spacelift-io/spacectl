c90c25029326faa3774c9b66b710003a624999c6
Marcin Białoń <marcin@bialon.net>
2023-01-17 17:14:18 +0100

    Extract user profile manager access to a function

--- client/session/from_current_profile.go
+++ client/session/from_current_profile.go
@@ -5,20 +5,13 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 // FromCurrentProfile creates a session from credentials stored in the currently selected profile.
 func FromCurrentProfile(ctx context.Context, client *http.Client) (Session, error) {
-	userHomeDir, err := os.UserHomeDir()
+	manager, err := UserProfileManager()
 	if err != nil {
-		return nil, fmt.Errorf("could not find user home directory: %w", err)
-	}
-
-	manager, err := NewProfileManager(filepath.Join(userHomeDir, SpaceliftConfigDirectory))
-	if err != nil {
-		return nil, fmt.Errorf("could not create profile manager: %w", err)
+		return nil, fmt.Errorf("could not access profile manager: %w", err)
 	}
 
 	currentProfile := manager.Current()
