55186821e4ed859e274cec6d621f024106cea4e5
Matt Calhoun <matt@mattcalhoun.com>
2023-06-15 18:21:49 -0400

    allow bind port to be set by env var

--- internal/cmd/profile/flags.go
+++ internal/cmd/profile/flags.go
@@ -6,4 +6,5 @@ var flagBindPort = &cli.IntFlag{
 	Name:     "bind",
 	Usage:    "[Optional] specify the port used for binding the server when logging in through a browser",
 	Required: false,
+	EnvVars:  []string{"SPACECTL_BIND_PORT"},
 }
