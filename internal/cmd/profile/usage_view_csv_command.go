f8ad2ebad5b73f7dbea8cd0c33b7be6a95a5dfaf
Michal Wasilewski <michal@mwasilewski.net>
2024-01-25 13:49:28 +0100

    fix(usage-csv): changes from review
    Signed-off-by: Michal Wasilewski <michal@mwasilewski.net>

--- internal/cmd/profile/usage_view_csv_command.go
+++ internal/cmd/profile/usage_view_csv_command.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -15,16 +14,9 @@ import (
 	"github.com/spacelift-io/spacectl/internal/cmd/authenticated"
 )
 
-type queryArgs struct {
-	since   string
-	until   string
-	aspect  string
-	groupBy string
-}
-
 func usageViewCSVCommand() *cli.Command {
 	return &cli.Command{
-		Name:      "usage-view-csv",
+		Name:      "usage-csv",
 		Usage:     "Prints CSV with usage data for the current account",
 		ArgsUsage: cmd.EmptyArgsUsage,
 		Flags: []cli.Flag{
@@ -35,73 +27,69 @@ func usageViewCSVCommand() *cli.Command {
 			flagUsageViewCSVFile,
 		},
 		Before: authenticated.Ensure,
-		Action: func(ctx *cli.Context) error {
-			// prep http query
-			args := &queryArgs{
-				since:   ctx.String(flagUsageViewCSVSince.Name),
-				until:   ctx.String(flagUsageViewCSVUntil.Name),
-				aspect:  ctx.String(flagUsageViewCSVAspect.Name),
-				groupBy: ctx.String(flagUsageViewCSVGroupBy.Name),
-			}
-			params := buildQueryParams(args)
-			req, err := http.NewRequestWithContext(ctx.Context, http.MethodGet, "/usageanalytics/csv", nil)
-			if err != nil {
-				return fmt.Errorf("failed to create an HTTP request: %w", err)
-			}
-			q := req.URL.Query()
-			for k, v := range params {
-				q.Add(k, v)
-			}
-			req.URL.RawQuery = q.Encode()
-
-			// execute http query
-			log.Println("Querying Spacelift for usage data...")
-			resp, err := authenticated.Client.Do(req)
-			if err != nil {
-				return err
-			}
-			defer resp.Body.Close()
-			if resp.StatusCode != http.StatusOK {
-				return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-			}
-
-			// save response to a file
-			var filePath string
-			if !ctx.IsSet(flagUsageViewCSVFile.Name) {
-				basePath, err := os.Getwd()
-				if err != nil {
-					return fmt.Errorf("failed to get base path: %w", err)
-				}
-				filePath = fmt.Sprintf(basePath+"/usage-%s-%s-%s.csv", args.aspect, args.since, args.until)
-			} else {
-				filePath = ctx.String(flagUsageViewCSVFile.Name)
-			}
-			fd, err := os.OpenFile(filepath.Clean(filePath), os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0600)
-			if err != nil {
-				return fmt.Errorf("failed to open a file descriptor: %w", err)
-			}
-			defer fd.Close()
-			bfd := bufio.NewWriter(fd)
-			defer bfd.Flush()
-			_, err = io.Copy(bfd, resp.Body)
-			if err != nil {
-				return fmt.Errorf("failed to write the response to a file: %w", err)
-			}
-			log.Println("Usage data saved to", filePath)
-			return nil
-		},
+		Action: usageViewCsv,
 	}
 }
 
-func buildQueryParams(args *queryArgs) map[string]string {
+func usageViewCsv(ctx *cli.Context) error {
+	// prep http query
+	params := buildQueryParams(ctx)
+	req, err := http.NewRequestWithContext(ctx.Context, http.MethodGet, "/usageanalytics/csv", nil)
+	if err != nil {
+		return fmt.Errorf("failed to create an HTTP request: %w", err)
+	}
+	q := req.URL.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	// execute http query
+	fmt.Fprint(os.Stderr, "Querying Spacelift for usage data...\n")
+	resp, err := authenticated.Client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	// process a response
+	var filePath string
+	if ctx.IsSet(flagUsageViewCSVFile.Name) {
+		filePath = ctx.String(flagUsageViewCSVFile.Name)
+		fd, err := os.OpenFile(filepath.Clean(filePath), os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0600)
+		if err != nil {
+			return fmt.Errorf("failed to open a file descriptor: %w", err)
+		}
+		defer fd.Close()
+		bfd := bufio.NewWriter(fd)
+		defer bfd.Flush()
+		_, err = io.Copy(bfd, resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to write the response to a file: %w", err)
+		}
+	} else {
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to write the response to stdout: %w", err)
+		}
+	}
+
+	fmt.Fprint(os.Stderr, "Usage data downloaded successfully.\n")
+	return nil
+}
+
+func buildQueryParams(ctx *cli.Context) map[string]string {
 	params := make(map[string]string)
 
-	params["since"] = args.since
-	params["until"] = args.until
-	params["aspect"] = args.aspect
+	params["since"] = ctx.String(flagUsageViewCSVSince.Name)
+	params["until"] = ctx.String(flagUsageViewCSVUntil.Name)
+	params["aspect"] = ctx.String(flagUsageViewCSVAspect.Name)
 
-	if args.aspect == "run-minutes" {
-		params["groupBy"] = args.groupBy
+	if ctx.String(flagUsageViewCSVAspect.Name) == "run-minutes" {
+		params["groupBy"] = ctx.String(flagUsageViewCSVGroupBy.Name)
 	}
 
 	return params
