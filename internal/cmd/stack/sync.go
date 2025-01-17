7b9218e2d5964a0508a354e589777779301a19ed
Jakub <jakub.wojtowicz@sephcode.pl>
2025-01-17 08:30:43 +0100

    feat: added stack sync-commit subcommand
    Signed-off-by: Jakub <jakub.wojtowicz@sephcode.pl>

---
+++ internal/cmd/stack/sync.go
@@ -0,0 +1,31 @@
+package stack
+
+import (
+	"fmt"
+
+	"github.com/shurcooL/graphql"
+	"github.com/spacelift-io/spacectl/internal/cmd/authenticated"
+	"github.com/urfave/cli/v2"
+)
+
+func syncCommit(cliCtx *cli.Context) error {
+	stackID, err := getStackID(cliCtx)
+	if err != nil {
+		return err
+	}
+
+	if nArgs := cliCtx.NArg(); nArgs != 0 {
+		return fmt.Errorf("expected zero arguments but got %d", nArgs)
+	}
+
+	var mutation struct {
+		Stack struct {
+			ID string `graphql:"id"`
+		} `graphql:"stackSyncCommit(id: $stack)"`
+	}
+	variables := map[string]interface{}{
+		"stack": graphql.ID(stackID),
+	}
+
+	return authenticated.Client.Mutate(cliCtx.Context, &mutation, variables)
+}
