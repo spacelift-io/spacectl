d45ab8b18e5b88899869f675521a2f3093962802
Michal Sokolowski <0michalsokolowski0@gmail.com>
2025-02-21 13:20:11 +0100

    chore: Remove unnecessary fields from input struct

--- internal/cmd/stack/inputs.go
+++ internal/cmd/stack/inputs.go
@@ -2,24 +2,5 @@ package stack
 
 // RuntimeConfigInput represents the input for triggering a run with a runtime configuration.
 type RuntimeConfigInput struct {
-	Yaml          *string        `json:"yaml"`
-	ProjectRoot   *string        `json:"projectRoot"`
-	RunnerImage   *string        `json:"runnerImage"`
-	Environment   *[]EnvVarInput `json:"environment"`
-	AfterApply    *[]string      `json:"afterApply"`
-	AfterDestroy  *[]string      `json:"afterDestroy"`
-	AfterInit     *[]string      `json:"afterInit"`
-	AfterPerform  *[]string      `json:"afterPerform"`
-	AfterPlan     *[]string      `json:"afterPlan"`
-	AfterRun      *[]string      `json:"afterRun"`
-	BeforeApply   *[]string      `json:"beforeApply"`
-	BeforeDestroy *[]string      `json:"beforeDestroy"`
-	BeforeInit    *[]string      `json:"beforeInit"`
-	BeforePerform *[]string      `json:"beforePerform"`
-	BeforePlan    *[]string      `json:"beforePlan"`
-}
-
-type EnvVarInput struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
+	Yaml *string `json:"yaml"`
 }
