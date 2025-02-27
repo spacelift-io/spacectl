1e08ec92a0d73fe5ee9368121fd34e625e91e376
ilya-hontarau <ilyah@spacelift.io>
2025-02-27 14:36:36 +0100

    fix: goimports

--- internal/cmd/provider/create_version.go
+++ internal/cmd/provider/create_version.go
@@ -8,11 +8,10 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/shurcooL/graphql"
+	"github.com/spacelift-io/spacectl/client/structs"
 	"github.com/spacelift-io/spacectl/internal/cmd/authenticated"
 	"github.com/spacelift-io/spacectl/internal/cmd/provider/internal"
 	"github.com/urfave/cli/v2"
-
-	"github.com/spacelift-io/spacectl/client/structs"
 )
 
 func createVersion() cli.ActionFunc {
