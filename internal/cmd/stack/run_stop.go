d879ffcf8edc29b6c4044a32f0c561b5ce84829e
Tomas <40318863+tomasmik@users.noreply.github.com>
2025-09-26 12:12:57 +0300

    Add a new "stack stop" command to stop runs (#347)

---
+++ internal/cmd/stack/run_stop.go
@@ -0,0 +1,55 @@
+package stack
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/shurcooL/graphql"
+	"github.com/urfave/cli/v3"
+
+	"github.com/spacelift-io/spacectl/internal/cmd/authenticated"
+)
+
+func runStop() cli.ActionFunc {
+	return func(ctx context.Context, cliCmd *cli.Command) error {
+		stackID, err := getStackID(ctx, cliCmd)
+		if err != nil {
+			return err
+		}
+
+		var mutation struct {
+			RunStop struct {
+				ID string `graphql:"id"`
+			} `graphql:"runStop(stack: $stack, run: $run, note: $note)"`
+		}
+
+		variables := map[string]interface{}{
+			"stack": graphql.ID(stackID),
+			"run":   graphql.ID(cliCmd.String(flagRequiredRun.Name)),
+			"note":  graphql.String("Stopped by spacectl"),
+		}
+
+		if err := authenticated.Client.Mutate(ctx, &mutation, variables); err != nil {
+			return err
+		}
+
+		fmt.Println("You have successfully attempted to stop the run")
+
+		fmt.Println("The run can be visited at", authenticated.Client.URL(
+			"/stack/%s/run/%s",
+			stackID,
+			mutation.RunStop.ID,
+		))
+
+		if !cliCmd.Bool(flagTail.Name) {
+			return nil
+		}
+
+		terminal, err := runLogsWithAction(ctx, stackID, mutation.RunStop.ID, nil)
+		if err != nil {
+			return err
+		}
+
+		return terminal.Error()
+	}
+}
